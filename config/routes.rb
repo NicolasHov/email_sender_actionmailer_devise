600a19faa50baaf33465f3ea732485e055a5250c
Nicolas Hovart <kola.hov@gmail.com>
2017-12-08 12:57:52 +0100

    email

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  root to: "home#index"  
+
+  root 'home#index'
+  
+  # DEVISE USERS
+  devise_for :users, controllers: { registrations: 'registrations' }, skip: [:registrations, :sessions]
+  devise_scope :user do
+    get 'signup', to: 'registrations#new', as: :new_user_registration
+    post 'signup', to: 'registrations#create', as: :user_registration
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
+  # USERS
+  # get 'users/:id', to: 'users#show', as: :user
+  # get 'users/:id/edit', to: 'users#edit', as: :edit_user_page
+  # post 'users/:id/edit', to: 'users#update', as: :update_user_page
 end
